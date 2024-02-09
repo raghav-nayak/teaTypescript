651cda80bcabcc79965b5fc3264ea5e09237adf3
Raghav Nayak <ragsnayak@gmail.com>
2024-02-10 01:30:06 +0530

    added number, boolean and type inference notes

--- src/02_variables.ts
+++ src/02_variables.ts
@@ -9,4 +9,19 @@ greetings.toUpperCase()
 
 greetings.tolowerCase() // error - Property 'tolowerCase' does not exist on type 'string'. Did you mean 'toLowerCase'?ts(2551)ˀ
 
+
+
+// number
+let userId: number = 334455.3;
+
+userId.toExponential()
+
+// boolean
+let isLoggedIn: boolean = false;
+
+// type inference
+let someNumber = 10.3;
+
+someNumber.toExponential(); // no error as ts infers the type based on the value assigned
+
 export { };
