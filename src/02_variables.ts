f68133b8a328ca311f874f63301179f0f02cc3f9
Raghav Nayak <ragsnayak@gmail.com>
2024-02-10 01:14:34 +0530

    added data types notes

---
+++ src/02_variables.ts
@@ -0,0 +1,12 @@
+let greetings: string = "Hello";
+console.log(greetings);
+
+let myNum = 6;
+
+myNum.toUpperCase() // error - Property 'toUpperCase' does not exist on type 'number'.ts(2339)
+
+greetings.toUpperCase()
+
+greetings.tolowerCase() // error - Property 'tolowerCase' does not exist on type 'string'. Did you mean 'toLowerCase'?ts(2551)ˀ
+
+export { };
